feb2018c77c54192da194e007e4b582df78ec540
miguel <miguel_3396@hotmail.es>
2023-05-28 03:13:30 +0200

    Agregando proyecto Java Proyecto_Ejemplo

---
+++ src/a.java
@@ -0,0 +1,23 @@
+
+public class a {
+
+	
+	int num1;
+	int num3;
+	int num5;
+	
+	public a(int num1, int num3, int num5) {
+		this.num1 = num1;
+		this.num3 = num3;
+		this.num5 = num5;
+	}
+	
+	
+public void sumar_primeros () {
+		
+		int suma;
+		suma=num1+num3;
+		System.out.println(suma);
+	}
+}
+
---
+++ src/b.java
@@ -0,0 +1,12 @@
+
+public class b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		a objeto1 = new a (1,2,3);
+
+		objeto1.sumar_primeros();
+	}
+
+}
